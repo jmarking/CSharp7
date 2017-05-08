f3a2afff1c5c6318a45968f40b9493780eee3c7e
Jon Marking <jon.marking@hotmail.com>
2017-05-08 07:58:26 -0500

    Add project files.

---
+++ CSharp7/Features/OutVariables.cs
@@ -0,0 +1,45 @@
+﻿
+namespace CSharp7.NewFeatures
+{
+    public class OutVariables
+    {
+        /// <summary>
+        /// out variable had to be explicitly declared outside of the out usage
+        /// </summary>
+        public string OldoutVariableUsage(string value)
+        {
+            int intValue; // Explicitly declared (int)
+            if (int.TryParse(value, out intValue))
+            {
+                return $"Successfully parsed int value {intValue}";
+            }
+			else
+            {
+                return $"Unable to parse \"{value}\" to an int";
+            }
+        }
+
+        /// <summary>
+        /// out variable can be implicitly declared inline of the out usage.
+        /// Can access out variable anywhere in local block after it's initialized.
+        /// </summary>
+        public string NewoutVariableUsage(string value)
+        {
+            var isInt = false;
+
+            if (int.TryParse(value, out var intValue)) // implicitly declared (var)
+            {
+                isInt = true;
+            }
+
+            if (isInt)
+            {
+                return $"Successfully parsed int value {intValue}"; // Access out variable anywhere in local block
+            }
+			else
+            {
+                return $"Unable to parse \"{value}\" to an int";
+            }
+        }
+    }
+}
---
+++ CSharp7/Program.cs
@@ -0,0 +1,28 @@
+﻿using CSharp7.NewFeatures;
+using System;
+
+namespace CSharp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseOutVariables();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void UseOutVariables()
+        {
+            var outVariables = new OutVariables();
+
+            Console.WriteLine("Enter int value to try parse: ");
+            var input = Console.ReadLine();
+
+            Console.WriteLine("\nBoth usages will still provide same result as expected.");
+            Console.WriteLine($"Old 'out' variable parse result: {outVariables.OldoutVariableUsage(input)}");
+            Console.WriteLine($"New 'out' variable parse result: {outVariables.NewoutVariableUsage(input)}\n");
+        }
+    }
+}
